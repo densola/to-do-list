53d33b0820648a6342420c6737f5aca3b7ba392a
densola <107649045+densola@users.noreply.github.com>
2024-10-19 18:49:36 -0500

    todo-list: Create react task component with styling

--- todo-list/frontend/app/page.tsx
+++ todo-list/frontend/app/page.tsx
@@ -1,11 +1,25 @@
 export default function Home() {
     return (
-        <div>
+        <div id="root">
             {/* TODO - don't hardcode url*/}
-            <form action="http://localhost:8000/" method="post">
+            {/* <form action="http://localhost:8000/" method="post">
                 <input name="txt" type="text" />
                 <button type="submit">Submit</button>
-            </form>
+                </form> */}
+            <button className="btn-add">Add task</button>
+            <MyTask name="Title for task" info="More details provided on task at hand."></MyTask>
         </div>
     );
+}
+
+function MyTask(task: { name: string, info: string }) {
+    return (
+        <div className="task">
+            <div className="task__header">
+                <input className="task__checkbox" type="checkbox" />
+                <h5 className="task__title">{task.name}</h5>
+            </div>
+            <p className="task__desc">{task.info}</p>
+        </div>
+    )
 }
