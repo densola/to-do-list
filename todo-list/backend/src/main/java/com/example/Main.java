85acb348ebf362b53b14938a23f2d996bf1beb96
densola <107649045+densola@users.noreply.github.com>
2024-10-20 18:54:03 -0500

    todo-list: Remove java index handler

--- todo-list/backend/src/main/java/com/example/Main.java
+++ todo-list/backend/src/main/java/com/example/Main.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.sql.Connection;
@@ -56,35 +55,9 @@ public class Main {
             System.exit(1);
         }
 
-        s.createContext("/", new HandleIndex());
         s.createContext("/getTasks", new HandleGetTasks());
 
         return s;
-    }
-
-    static class HandleIndex implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            StringBuilder request = new StringBuilder(); // part of response
-            StringBuilder response = new StringBuilder(); // sent as full response
-            InputStream ios = t.getRequestBody();
-
-            int i;
-            while ((i = ios.read()) != -1) {
-                String s = String.format("%d is %s\n", i, (char) i);
-                request.append((char) i);
-                response.append(s);
-            }
-
-            response.append("\nYour request was: ").append(request.toString());
-
-            OutputStream os = t.getResponseBody();
-            t.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
-            t.sendResponseHeaders(200, response.length());
-
-            os.write(response.toString().getBytes());
-            os.close();
-        }
     }
 
     static class HandleGetTasks implements HttpHandler {
